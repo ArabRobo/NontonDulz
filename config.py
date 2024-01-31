1ee5362d58ebd7c394651b80a7d10e91e1e5221b
ArabRobo <121577164+ArabRobo@users.noreply.github.com>
2024-02-01 02:50:43 +0700

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://ntpbwdav:qsJcJm0ntbzlPLiCrXZwnFvIak9wktTm@rain.db.elephantsql.com/ntpbwdav")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001537697834"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001953065740"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002100850265"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001632945709"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
